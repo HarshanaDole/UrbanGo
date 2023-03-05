40b62b5029424bf5019cfa1f0b7bf6372a8fcc8f
Harshana <harudole@gmail.com>
2023-03-05 21:46:37 +0530

    First Commit

---
+++ admin/SideNavBar.php
@@ -0,0 +1,95 @@
+
+    <nav class="sideB close">
+        <header>
+            <div class="image-text">
+                <span class="image">
+                    
+                </span>
+
+                <div class="text logo-text">
+                    <span class="name">UrbanGo</span>
+                    <span class="profession">Admin Panel</span>
+                </div>
+            </div>
+
+            <i class='bx bx-chevron-right toggle'></i>
+        </header>
+
+        <div class="menu-bar">
+            <div class="menu">
+
+                <li class="search-box">
+                    <i class='bx bx-search icon'></i>
+                    <input type="text" placeholder="Search...">
+                </li>
+
+                <ul class="menu-links">
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-home-alt icon' ></i>
+                            <span class="text nav-text">Dashboard</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-bar-chart-alt-2 icon' ></i>
+                            <span class="text nav-text">Buses</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-bell icon'></i>
+                            <span class="text nav-text">Trips</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-pie-chart-alt icon' ></i>
+                            <span class="text nav-text">Sessions</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-heart icon' ></i>
+                            <span class="text nav-text">Bookings</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-link">
+                        <a href="#">
+                            <i class='bx bx-wallet icon' ></i>
+                            <span class="text nav-text">Routes</span>
+                        </a>
+                    </li>
+
+                </ul>
+            </div>
+
+            <div class="bottom-content">
+                <li class="">
+                    <a href="#">
+                        <i class='bx bx-log-out icon' ></i>
+                        <span class="text nav-text">Logout</span>
+                    </a>
+                </li>
+
+                <li class="mode">
+                    <div class="sun-moon">
+                        <i class='bx bx-moon icon moon'></i>
+                        <i class='bx bx-sun icon sun'></i>
+                    </div>
+                    <span class="mode-text text">Dark mode</span>
+
+                    <div class="toggle-switch">
+                        <span class="switch"></span>
+                    </div>
+                </li>
+                
+            </div>
+        </div>
+
+    </nav>
---
+++ admin/index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+  
+<html lang="en">
+<head>
+
+/* CSS */
+
+    <link rel="stylesheet" href="css/style.css">
+    
+/* Boxicons CSS */
+
+    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+    
+/* <title>Admin Panel Dashboard</title> */
+
+</head>
+<body>
+    
+    <?php include "SideNavBar.php"; ?>
+
+    <section class="home">
+        <div class="text">Admin Panel</div>
+
+        <div class="box-container">
+
+            <div class="box">
+                <h3>16</h3>
+                <p>buses added</p>
+                <a href="" class="btn">see buses</a>
+            </div>
+
+            <div class="box">
+                <h3>60</h3>
+                <p>trips assigned</p>
+                <a href="" class="btn">see trips</a>
+            </div>
+
+            <div class="box">
+                <h3>4</h3>
+                <p>ongoing sessions</p>
+                <a href="" class="btn">see sessions</a>
+            </div>
+
+            <div class="box">
+                <h3>12</h3>
+                <p>routes & fares</p>
+                <a href="" class="btn">see routes</a>
+            </div>
+
+            <div class="box">
+                <h3>60</h3>
+                <p>bookings added</p>
+                <a href="" class="btn">see bookings</a>
+            </div>
+
+            <div class="box">
+                <h3>54</h3>
+                <p>feedback recieved</p>
+                <a href="" class="btn">see feedback</a>
+            </div>
+
+            <div class="box">
+                <h3>69</h3>
+                <p>users registered</p>
+                <a href="" class="btn">see users</a>
+            </div>
+
+            <div class="box">
+                <h3>23</h3>
+                <p>admins registered</p>
+                <a href="" class="btn">see admins</a>
+            </div>
+
+        </div>
+
+
+    </section>
+
+    <script src="js/script.js"></script>
+
+</body>
+</html>
